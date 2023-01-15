7279379580d3405b3889dd569c9b18ebe35441fb
aleksandra <alexandra.bulgak@gmail.com>
2023-01-15 23:06:19 +0200

    Task 029 was added

---
+++ Task_029/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+
+
+int[] Array(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(0, 100);
+    }
+
+    return result;
+}
+
+int[] array = Array(8);
+Console.WriteLine($"[{String.Join(", ", array)}]");
